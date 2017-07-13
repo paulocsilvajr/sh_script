69ae263efac3dfd2d525ff308c8e7cf33ce53679
paulocsilvajr <pauluscave@gmail.com>
2017-07-12 22:17:56 -0300

    Incrementado ajuda em backup_bookmark_firefox

--- backup_bookmark_firefox.sh
+++ backup_bookmark_firefox.sh
@@ -6,6 +6,19 @@ DIR_USER_FIREFOX=$(ls $DIR_BASE | grep '.default')
 DIR_ORIGEM=$DIR_BASE$DIR_USER_FIREFOX"/bookmarkbackups"
 DIR_BACKUP="/home/paulo/pc/bookmark_"$USER
 
+ajuda(){
+    echo "Script elaborado para efetuar backup de /var/www sem interferência humana através do cron."
+    echo "Sintaxe: $0 [ -h | --help ]"
+    echo "-h --help      ajuda"
+
+    exit 
+}
+
+case $1 in
+    "-h"|"--help")
+        ajuda;;
+esac
+
 if [ ! -d "$DIR_BACKUP" ]; then
     mkdir "${DIR_BACKUP}" 
 fi
