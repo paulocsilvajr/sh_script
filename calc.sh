c77f030f5ff3f07ceea650f81792f07ecb25e4f8
paulocsilvajr <pauluscave@gmail.com>
2017-07-12 22:33:31 -0300

    Ajuda e refatoração de calc.sh

--- calc.sh
+++ calc.sh
@@ -5,15 +5,17 @@ case $1 in
         echo 'Informe um cálculo como parâmetro ou -h para ajuda'
         ;;
     "-h"|"--help")
-        echo 'Cálculos matemáticos pelo terminal.'
+        echo "Cálculos matemáticos pelo terminal."
         echo "O cálculo devem estar entre aspas simples e não conter espaços"
-        echo "Ex: calc '1+1'     =>     2"
-        echo "    calc '2*(2+4)' =>    16"
-        echo "    calc '1*2,2*3' => (2,6)"
+        echo "Sintaxe: $0 'operacao'"
+        echo "         $0 [ -h | --help ]"
+        echo "Ex:      $0 '1+1'     =>     2"
+        echo "         $0 '2*(2+4)' =>    16"
+        echo "         $0 '1*2,2*3' => (2,6)"
         ;;
     *)
         # cálculo efetuado pelo python, com direcionamento de erro
         # para /dev/null e tratamento da mensagem de erro
-        python -c "print($1)" 2> /dev/null || echo 'Cálculo inválido'
+        python3 -c "print($1)" 2> /dev/null || echo 'Cálculo inválido'
         ;;
 esac
